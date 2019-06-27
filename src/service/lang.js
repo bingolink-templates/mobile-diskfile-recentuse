9937932c49ac943dd5feb706058834b8253b9d86
huanghaojie <13612989029@163.com>
2019-06-27 13:43:48 +0800

    更换为更多, 高度通知多次

--- src/service/lang.js
+++ src/service/lang.js
@@ -1,19 +1,19 @@
 module.exports = {
     "zh-cn": {
         "RecentlyUsedFile": "最近使用文档",
-        "All": "全部",
+        "All": "更多",
         "NoneData": "暂无文档",
         "ErrorLoadData": "加载失败"
     },
     "zh-hk": {
         "RecentlyUsedFile": "最近使用文檔",
-        "All": "全部",
+        "All": "更多",
         "NoneData": "暫無檔案",
         "ErrorLoadData": "加載失敗"
     },
     "en": {
-        "RecentlyUsedFile": "Recently Used File",
-        "All": "All",
+        "RecentlyUsedFile": "Recently used file",
+        "All": "More",
         "NoneData": "No archives data",
         "ErrorLoadData": "loading failed"
     }
